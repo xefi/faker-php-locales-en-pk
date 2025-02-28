717706abeb572322aa7b1b49df263cae2c7794e2
Martin Soenen <martin.soenen@outlook.fr>
2025-02-28 10:09:42 +0100

    First commit

---
+++ src/FakerEnPkServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Xefi\Faker\EnPk;
+
+use Xefi\Faker\Providers\Provider;
+
+class FakerEnPkServiceProvider extends Provider
+{
+    public function boot(): void
+    {
+        $this->extensions([]);
+    }
+}
